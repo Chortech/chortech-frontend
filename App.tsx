dca31f52e243229da48404284ea1fd6a6085ce96
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-10-26 21:48:48 +0330

    Migrate to typescript

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react'
+import { Component } from 'react'
+import { View, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+};
--- metro.config.js
+++ metro.config.js
@@ -13,5 +13,7 @@ module.exports = {
         inlineRequires: false,
       },
     }),
+    babelTransformerPath: require.resolve('react-native-typescript-transformer')
   },
 };
+
