972242370da06f9fc297dededa054533744de92d
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2020-10-28 14:42:52 +0330

    add profile styles

---
+++ app/styles/ProfileStyles.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1AD927',
+    },
+    imageViewStyle: {
+        backgroundColor: '#ffffff',
+        alignItem: 'center',
+        height: 60,
+        paddingTop: 15,
+    },
+
+    textStyle: {
+        fontSize: 20,
+    }
+})
