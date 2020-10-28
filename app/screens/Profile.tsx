2cea10c736fe43ef54537bf794cdeddd12d7b15c
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2020-10-28 20:52:59 +0330

    add buttons and texts, modify the whole screen

--- app/screens/Profile.tsx
+++ app/screens/Profile.tsx
@@ -3,22 +3,46 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { styles } from '../styles/ProfileStyles';
 
 const ProfileScreen = () => {
 
-    const { container, textStyle, userNameTextStyle, headerStyle, profileImageStyle } = styles;
+    const {
+        container,
+        textStyle,
+        userNameTextStyle,
+        headerStyle,
+        profileImageStyle,
+        infoStyle,
+        buttonStyle,
+        buttonTextStyle,
+        textContainer } = styles;
 
     return (
     <View style={container}>
         <View style={headerStyle}>
             <Image style={profileImageStyle} source={require('../assets/images/profile-image-sample.png')}/>
-            <Text style={userNameTextStyle}>Andres Walker</Text>
+            <Text style={userNameTextStyle}>Babak1999</Text>
         </View>
-        <View>
-            <Text style={textStyle}>Full name: </Text>
-            <Text style={textStyle}>Email: </Text>
+        <View style={infoStyle}>
+            <View style={textContainer}>
+                <Text style={textStyle}>نام و نام خانوادگی</Text>
+            </View>
+            <View style={textContainer}>
+                <Text style={textStyle}>آدرس الکترونیک</Text>
+            </View>
+            <View style={textContainer}>
+                <Text style={textStyle}>شماره تلفن همراه</Text>
+            </View>
+            <View style={{marginTop: 100}}>
+                <TouchableOpacity style={buttonStyle}>
+                    <Text style={buttonTextStyle}>دوستان</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={buttonStyle}>
+                    <Text style={buttonTextStyle}>ویرایش اطلاعات</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     </View>
     );
