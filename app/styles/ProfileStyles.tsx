21b219aa541a30899d6749e91281b584436ab387
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2020-10-28 20:51:16 +0330

    add bottuns' styles and text styles

--- app/styles/ProfileStyles.tsx
+++ app/styles/ProfileStyles.tsx
@@ -4,33 +4,59 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        // adding flex is needed.
+        height: 800,
         backgroundColor: '#1AD927',
     },
     headerStyle: {
-        backgroundColor: '#1AD927',
         alignItems: 'center',
-        height: 265,
         paddingTop: 20,
     },
+    infoStyle: {
+        backgroundColor: '#fff',
+        height: 450,
+        paddingHorizontal: 20,
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30,
+    },
     profileImageStyle: {
-        height: 200,
-        width: 200,
-        borderRadius: 100,
+        height: 150,
+        width: 150,
+        borderRadius: 75,
     },
     userNameTextStyle: {
-        fontSize: 25,
-        paddingLeft: 20,
-        paddingRight: 20,
+        fontSize: 20,
+        marginVertical: 15,
         color: '#fff',
         alignItems: 'center',
         fontFamily: 'Dirooz',
     },
     textStyle: {
-        fontSize: 20,
-        paddingTop: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
+        fontSize: 18,
+        color: '#053751',
+        writingDirection: 'auto',
         fontFamily: 'Dirooz',
+    },
+    buttonStyle: {
+        elevation: 2,
+        backgroundColor: '#fff',
+        borderWidth: 2,
+        borderColor: '#1AD927',
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        marginBottom: 15,
+    },
+    buttonTextStyle: {
+        fontSize: 18,
+        color: '#1AD927',
+        alignSelf: 'center',
+        fontFamily: 'IRANSansWeb_Bold',
+        writingDirection: 'auto',
+    },
+    textContainer: {
+        padding: 20,
+        borderBottomColor: '#EBE8E8',
+        borderBottomWidth: 1,
     },
 });
