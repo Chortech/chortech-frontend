48f8fc10cce7f63905375b453ce9bb12d360bf18
Babak Sefidgar <babak.sefidgar@gmail.com>
2020-11-08 17:02:49 +0330

    remove navigation initializing from App.tsx to navigation folder and define rootnavigation store and persist memory for simplicity

--- App.tsx
+++ App.tsx
@@ -1,53 +1,36 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+
+import Navigator from '@navigation/navigationStack';
+import configureStore from './app/store';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
-import SignUp from './app/screens/SignUp/index';
-import Login from './app/screens/Login/index';
-import CodeVerification from './app/screens/CodeVerification/index';
-import Profile from './app/screens/Profile/index';
-import EditProfile from './app/screens/EditProfile/index';
-import ResetPassword from './app/screens/ResetPassword/index';
-import CreditCardList from './app/screens/CreditCardList/index';
 
 library.add(fab, fas);
 
-type RootStackParamList = {
-	Login: undefined;
-	SignUp: undefined;
-	CodeVerification: undefined;
-	Profile: undefined;
-	EditProfile: undefined;
-	ResetPassword: undefined;
-	CreditCardList: undefined;
-};
+const { persistor, store } = configureStore();
 
-const Stack = createStackNavigator<RootStackParamList>();
-
-const App = (): void => {
+// initializing navigation and redux store and persist 
+const RootNavigation: React.FC = () => {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				initialRouteName="Login"
-				screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="Login" component={Login} />
-				<Stack.Screen name="SignUp" component={SignUp} />
-				<Stack.Screen
-					name="CodeVerification"
-					component={CodeVerification} />
-				<Stack.Screen name="Profile" component={Profile} />
-				<Stack.Screen name="EditProfile" component={EditProfile} />
-				<Stack.Screen name="ResetPassword" component={ResetPassword} />
-				<Stack.Screen name="CreditCardList" component={CreditCardList} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<Navigator/>
 	);
-};
-
-export default App;
+  };
+  
+  const App: React.FC = () => {
+	return (
+	  <Provider store={store}>
+		<PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+		  <RootNavigation />
+		</PersistGate>
+	  </Provider>
+	);
+  };
+  
+  export default App;
