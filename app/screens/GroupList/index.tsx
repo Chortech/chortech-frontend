2f8dd3b130ad732fa8c560b739ae9505392163c2
Babak Sefidgar <babak.sefidgar@gmail.com>
2020-11-12 01:44:43 +0330

    make base files for group ui

---
+++ app/screens/GroupList/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const GroupList: React.FC = () => {
+  return (
+    <View>
+        <Text>Group List</Text>
+    </View>
+  );
+};
+
+export default GroupList;
