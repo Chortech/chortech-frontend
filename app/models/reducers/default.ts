66322234ec6b6d9f8dc9ee341d55c1f4cd90d878
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-24 19:57:41 +0330

    Add friend model

---
+++ app/models/reducers/default.ts
@@ -0,0 +1,11 @@
+export interface IUserState {
+  loading: boolean;
+  id: string;
+  name: string;
+  password: string;
+  email: string;
+  phone: string;
+  credit: number;
+  balance: number;
+  friends: Array<Friend>;
+}
