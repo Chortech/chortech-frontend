599feb9c61a87cb44183caec134a46e43de09541
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-24 21:55:53 +0330

    Add onUserFriendsResponse action to friendActinos

--- app/store/actions/friendActions.ts
+++ app/store/actions/friendActions.ts
@@ -1,12 +1,26 @@
 import * as types from "./types";
 import { Action } from "../../models/actions/action";
 import { FriendsRequest } from "../../models/requests/getFriends";
+import { FriendsResponse } from "../../models/responses/getFriends";
 
-export function GetUserFriends(userId: string): Action<FriendsRequest> {
+export function onUserFriendsRequest(userId: string): Action<FriendsRequest> {
   return {
     type: types.USER_FRIENDS_REQUEST,
     payload: {
       userId: userId,
+    },
+  };
+}
+
+export function onUserFriendsResponse(
+  response: FriendsResponse
+): Action<FriendsResponse> {
+  return {
+    type: types.USER_FRIENDS_RESPONSE,
+    payload: {
+      relationId: response.relationId,
+      userId: response.userId,
+      friends: response.friends,
     },
   };
 }
