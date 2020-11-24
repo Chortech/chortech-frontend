5793c69b989a4ac5d05c9c18abe0dd8c74fa7cdd
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-24 13:38:05 +0330

    Add props to friend item

--- app/components/FriendItem/index.tsx
+++ app/components/FriendItem/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { GestureResponderEvent, Image, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { styles } from "./styles";
 
-const FriendItem: React.FC = (): JSX.Element => {
+type Props = {
+  onPressFriendItem: (event: GestureResponderEvent) => void | undefined;
+  Name: string;
+  ImageUrl: string;
+};
+
+const FriendItem: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <View>
-      <TouchableOpacity style={styles.friendContainer}>
-        <Text style={styles.friendText}>Hello</Text>
-        <Image
-          style={styles.friendImage}
-          source={require("../../assets/images/friend-image.jpg")}
-        />
+      <TouchableOpacity
+        style={styles.friendContainer}
+        onPress={props.onPressFriendItem}>
+        <Text style={styles.friendText}>{props.Name}</Text>
+        <Image style={styles.friendImage} source={require(props.ImageUrl)} />
       </TouchableOpacity>
     </View>
   );
 };
+
+export default FriendItem;
