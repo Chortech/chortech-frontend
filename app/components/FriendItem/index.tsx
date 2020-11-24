8436b4dd732fd4752a1271e035e014eb1db62836
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-24 13:31:14 +0330

    Add friend item styles

--- app/components/FriendItem/index.tsx
+++ app/components/FriendItem/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { styles } from "./styles";
 
 const FriendItem: React.FC = (): JSX.Element => {
   return (
     <View>
-      <TouchableOpacity>
-        <Text>Hello</Text>
-        <Image source={require("../../assets/images/friend-image.jpg")} />
+      <TouchableOpacity style={styles.friendContainer}>
+        <Text style={styles.friendText}>Hello</Text>
+        <Image
+          style={styles.friendImage}
+          source={require("../../assets/images/friend-image.jpg")}
+        />
       </TouchableOpacity>
     </View>
   );
