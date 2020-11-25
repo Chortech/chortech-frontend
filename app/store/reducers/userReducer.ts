3118a3e264e8cf77703af4788414d1a0f4c85f00
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-11-26 02:20:02 +0330

    Init userReducer, add fetch user type to user reducer

---
+++ app/store/reducers/userReducer.ts
@@ -0,0 +1,31 @@
+import createReducer from "../../lib/createReducer";
+import { Action } from "../../models/actions/action";
+import { IUserState } from "../../models/reducers/default";
+import { FetchUserRequest } from "../../models/requests/getUser";
+import * as types from "../actions/types";
+
+const initialState: IUserState = {
+  loading: false,
+  id: "-1",
+  name: "",
+  password: "",
+  email: "",
+  phone: "",
+  credit: 0,
+  balance: 0,
+  friends: [],
+  groups: [],
+  activities: [],
+};
+
+export const userReducer = createReducer(initialState, {
+  [types.FETCH_USER_REQUEST](
+    state: IUserState,
+    action: Action<FetchUserRequest>
+  ) {
+    return {
+      ...state,
+      id: action.payload.id,
+    };
+  },
+});
