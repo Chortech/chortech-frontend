55852d789f870c3f59acbbca6fd0ecd6ef449e08
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2020-11-27 01:02:17 +0330

    add add-activity-request

--- app/models/requests/addActivityRequest.ts
+++ app/models/requests/addActivityRequest.ts
@@ -1,9 +1,7 @@
-import { Debt } from "../other/Debt";
-import { Expense } from "../other/Expense";
-
 export interface AddActivityRequest {
-  id: string;
+  userId: string;
   type: string;
-  expense?: Expense;
-  debt?: Debt;
+  groupId?: string;
+  expenseId?: string;
+  debtId?: string;
 }
