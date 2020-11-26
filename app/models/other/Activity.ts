00bf62b821b29816748aeb0fc1e78e2cc99a93d2
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2020-11-26 11:08:11 +0330

    modify activity's model

--- app/models/other/Activity.ts
+++ app/models/other/Activity.ts
@@ -1,10 +1,8 @@
 import { Debt } from "./Debt";
 import { Expense } from "./Expense";
-import { User } from "./User";
 
 export type Activity = {
   id: string;
-  user: User;
   type: string;
   expense?: Expense;
   debt?: Debt;
