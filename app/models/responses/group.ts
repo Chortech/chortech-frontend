7440f5bb0930ef9d24df8f4e2874c4f36a5de2c1
Babak Sefidgar <babak.sefidgar@gmail.com>
2020-11-26 11:37:25 +0330

    add group response layout

---
+++ app/models/responses/group.ts
@@ -0,0 +1,24 @@
+export interface addGroup {
+  id: string;
+  success: boolean;
+  }
+
+export interface updateGroup {
+  id: string;
+  success: boolean;
+  }
+
+export interface deleteGroup {
+  id: string;
+  success: boolean;
+  }
+
+export interface getGroupById {
+  id: string;
+  success: boolean;
+}
+
+export interface getUserGroups {
+  id: string;
+  success: boolean;
+}
