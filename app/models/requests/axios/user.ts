683aa8c2b51602772afe25e59a8559c2ca97bf65
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-12-21 17:49:35 +0330

    Add axios user request models

---
+++ app/models/requests/axios/user.ts
@@ -0,0 +1,37 @@
+import { Token } from "../../other/Token";
+
+export interface getUserFriendsRequest {
+  token: Token;
+}
+
+export interface addFriendEmailRequest {
+  email: string;
+}
+
+export interface addFriendPhoneRequest {
+  phone: string;
+}
+
+export interface removeFriendRequest {
+  id: string;
+}
+
+export interface invitee {
+  type: "phone" | "email" | "combo";
+  phone?: string;
+  email?: string;
+  name: string;
+}
+
+export interface inviteFriendsRequest {
+  invitees: Array<invitee>;
+}
+
+export interface getProfileRequest {
+  token: Token;
+}
+
+export interface editProfileRequest {
+  picture: string;
+  newName: string;
+}
