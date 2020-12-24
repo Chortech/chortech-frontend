e52c067ba51d6a82c5f3762152fac159accdb494
Babak Sefidgar <babak.sefidgar@gmail.com>
2020-12-25 00:43:20 +0330

    add onPressUpdateImage function to change profile image

--- app/screens/Profile/index.tsx
+++ app/screens/Profile/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, TouchableHighlight } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import * as Animatable from "react-native-animatable";
 import { useDispatch, useSelector, useStore } from "react-redux";
@@ -9,19 +9,45 @@ import * as authActions from "../../store/actions/authActions";
 import LoadingIndicator from "../Loading";
 import NavigationService from "../../navigation/navigationService";
 import { styles } from "./styles";
+import * as ImagePicker from 'react-native-image-picker';
 
 type IState = {
   userReducer: IUserState;
 };
 
+const options = {
+  title: 'Select Avatar',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+
 const Profile: React.FC = (): JSX.Element => {
   const loggedInUserId: string = useStore().getState()["authReducer"].id;
-  const user = useSelector((state: IState) => state.userReducer);
+  let user = useSelector((state: IState) => state.userReducer);
   const dispatch = useDispatch();
-
+  const [data, setData] = useState({
+    imageUri: user.imageUri,
+  });
   const fetchUser = () => {
     dispatch(userActions.onGetUserRequest(loggedInUserId));
   };
+  const onPressUpdateImage = () => {
+    let uri = "../../assets/images/friend-image.jpg";
+    ImagePicker.launchImageLibrary(options,
+      (response) => {
+        uri = response.uri;
+        setData({
+          ...data,
+          imageUri: uri,
+        })
+    user={
+      ...user,
+      imageUri: data.imageUri,
+    }
+    dispatch(userActions.onUpdateUserRequest(user));})
+  }
   const onPressFriendsList = () => NavigationService.navigate("FriendList");
   const onPressEditProfile = () => NavigationService.navigate("EditProfile");
   const onLogout = () => dispatch(authActions.onLogout());
@@ -37,10 +63,13 @@ const Profile: React.FC = (): JSX.Element => {
       ) : (
         <View style={styles.container}>
           <View style={styles.header}>
-            <Image
-              style={styles.profileImage}
-              source={require("../../assets/images/friend-image.jpg")}
-            />
+            <TouchableOpacity onPress={onPressUpdateImage}>
+              <Image
+                style={styles.profileImage}
+                source={{uri: data.imageUri}}
+              />
+            </TouchableOpacity>
+
             <TouchableOpacity style={styles.logoutIcon} onPress={onLogout}>
               <FontAwesomeIcon icon="sign-out-alt" style={{ color: "#ff0000" }} size={25} />
             </TouchableOpacity>
