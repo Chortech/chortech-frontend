44b77942a0b5d2e809500851c92eebdab6902164
Babak Sefidgar <babak.sefidgar@gmail.com>
2020-12-25 13:53:36 +0330

    add category filter ui to activity list

--- app/screens/ActivityList/index.tsx
+++ app/screens/ActivityList/index.tsx
@@ -7,6 +7,8 @@ import { IUserState } from "../../models/reducers/default";
 import { GetUserActivitiesResponse } from "../../models/responses/user";
 import NavigationService from "../../navigation/navigationService";
 import * as userActions from "../../store/actions/userActions";
+import SelectableItem from "../../components/SelectableItem";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import styles from "./styles";
 
 type IState = {
@@ -15,6 +17,17 @@ type IState = {
 
 const ActivityList: React.FC = () => {
   const loggedInUser: IUserState = useStore().getState()["authReducer"];
+  const [renderFlatList, setRenderFlatList] = useState(false);
+  const [data, setData] = useState({
+    categories: [{id:"0", selected:false,  name: "مواد غذایی", icon: "utensils"}, 
+                    {id:"1", selected:false,  name: "پوشاک", icon: "tshirt"}, 
+                    {id:"2", selected:false,  name: "هدیه", icon: "gift"},
+                    {id:"3", selected:false,  name: "سلامت", icon: "heartbeat"},
+                    {id:"4", selected:false,  name: "لوازم تحریر", icon: "pencil-ruler"},
+                    {id:"5", selected:false,  name: "ورزش", icon: "dumbbell"},
+                    {id:"6", selected:false,  name: "سفر", icon: "suitcase-rolling"},
+                    {id:"7", selected:false,  name: "کالای دیجیتال", icon: "laptop"}]
+  });
   const { activities } = useSelector((state: IState) => state.userReducer);
   const dispatch = useDispatch();
   const onPressActivityItem = (
@@ -47,9 +60,38 @@ const ActivityList: React.FC = () => {
     setRefreshing(false);
   }, [dispatch]);
 
+  const renderCategory: any = ({ item }) => (
+      <>
+    <SelectableItem
+      id={item.id}
+      Name={item.name}
+      selected={item.selected}
+      onPressItem={() => {data.categories[item.id].selected = !data.categories[item.id].selected;
+        setRenderFlatList(!renderFlatList);
+      }
+    }
+    />
+      <FontAwesomeIcon icon={item.icon} size={20} style={{top:30}}/>
+    </>
+  );  
+
   return (
     <>
       <View style={styles.container}>
+          <FlatList
+                horizontal={true}
+                data={data.categories}
+                renderItem={renderCategory}
+                extraData={renderFlatList}
+                scrollEnabled={true}
+                keyExtractor={item => item.id}
+                style={{
+                position: "relative",
+                backgroundColor: "white",
+                marginHorizontal: 0,
+                paddingBottom: -10,
+                marginBottom: -40,}}
+              />
         <Animatable.View animation="slideInUp" duration={600} style={styles.infoContainer}>
           <FlatList
             refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
