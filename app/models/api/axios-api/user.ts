c1fa1564d464fcb7154a66b12d92b830aa260294
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2020-12-25 18:05:51 +0330

    add expense api model

--- app/models/api/axios-api/user.ts
+++ app/models/api/axios-api/user.ts
@@ -1,14 +1,9 @@
-import { Token} from "../../other/axios/Token";
-import { Participant } from "../../other/axios/Participant";
 import { Response } from "../../responses/axios/response";
 import {
   AddFriend,
   GetUserFriends,
   DeleteFriend,
   UserProfileResponse,
-  AddExpense,
-  GetExpense,
-  AddComment,
 } from "../../responses/axios/user";
 
 export interface userApi {
@@ -19,14 +14,4 @@ export interface userApi {
   deleteFriend(friendId: string): Promise<Response<DeleteFriend>>;
   inviteFriendRequestByEmail(email: string): Promise<Response<null>>;
   inviteFriendRequestByPhone(phone: string): Promise<Response<null>>;
-  getExpense(): Promise<Response<GetExpense>>;
-  addExpense(
-    description: string,
-    total: number,
-    paid_at: number,
-    group: string,
-    notes: string,
-    participants: Array<Participant>
-  ): Promise<Response<AddExpense>>;
-  addComment(text: string, created_at: number): Promise<Response<AddComment>>;
 }
