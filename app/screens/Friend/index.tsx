ed8599de5461f4517cf41e18369a160577224786
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2021-01-22 20:41:58 +0330

    add settle up button

--- app/screens/Friend/index.tsx
+++ app/screens/Friend/index.tsx
@@ -10,6 +10,7 @@ import * as authActions from "../../store/actions/authActions";
 import { validateToken } from "../../utils/tokenValidator";
 import LoadingIndicator from "../Loading";
 import { styles } from "./styles";
+import NavigationService from "../../navigation/navigationService";
 
 type Props = {
   route: RouteProp<RootStackParamList, "Friend">;
@@ -21,9 +22,11 @@ type IState = {
 
 const Friend: React.FC<Props> = ({ route }: Props): JSX.Element => {
   const loggedInUser: IUserState = useStore().getState()["authReducer"];
-  const { id, friendName } = route.params;
+  // const { id, friendName } = route.params;
   const { loading } = useSelector((state: IState) => state.userReducer);
   const dispatch = useDispatch();
+
+  const onPressSettleUp = () => NavigationService.navigate("SettleUp");
 
   const onPressDeleteFriend = () => {
     if (validateToken(loggedInUser.token)) {
@@ -52,10 +55,13 @@ const Friend: React.FC<Props> = ({ route }: Props): JSX.Element => {
               style={styles.friendImage}
               source={require("../../assets/images/friend-image.jpg")}
             />
-            <Text style={styles.userNameText}>{friendName}</Text>
+            <Text style={styles.userNameText}></Text>
           </View>
           <Animatable.View animation="slideInUp" duration={600} style={styles.infoContainer}>
             <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.settleUpButton} onPress={onPressSettleUp}>
+                <Text style={styles.settleUpButtonText}>تسویه حساب</Text>
+              </TouchableOpacity>
               <TouchableOpacity style={styles.removeButton} onPress={onPressDeleteFriend}>
                 <Text style={styles.removeButtonText}>حذف کردن از دوستان</Text>
               </TouchableOpacity>
