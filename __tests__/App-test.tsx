357b5a1ec444db2f579683055e32f33574abdb62
Babak Sefidgar <babak.sefidgar@gmail.com>
2021-01-22 21:27:53 +0330

    add app render test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,10 +5,22 @@
 import "react-native";
 import React from "react";
 import App from "../App";
+import { act } from 'react-test-renderer';
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
 
-it("renders correctly", () => {
-  renderer.create(<App />);
+export async function wait(ms = 0) {
+  return await act(async () => {
+    return new Promise(resolve => {
+      setTimeout(resolve, ms);
+    });
+  });
+}
+
+jest.useFakeTimers()
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper')
+it("renders correctly", async () => {
+  const result = renderer.create(<App />);
+  await act(async () => { expect(result).toMatchSnapshot(); })
 });
