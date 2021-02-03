eb586b03f627ee9cbb43b660ef53ad66b1aece2f
Babak Sefidgar <babak.sefidgar@gmail.com>
2021-02-03 23:52:39 +0330

    add push notification ui and functions

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { ActivityIndicator, Alert } from "react-native";
+import { ActivityIndicator, Alert, View, Modal } from "react-native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -12,8 +12,9 @@ import { cronJob } from "./app/utils/cronJob";
 import * as authActions from "./app/store/actions/authActions";
 import { log } from "./app/utils/logger";
 import { IUserState } from "./app/models/reducers/default";
-import StartUp from "./app/screens/Startup";
-import { flush } from "redux-saga/effects";
+import { Root, Popup, Toast } from 'popup-ui'
+import notifee, { AndroidImportance } from '@notifee/react-native';
+import NavigationService from "./app/navigation/navigationService";
 
 library.add(fab, fas);
 
@@ -35,23 +36,44 @@ const RootNavigation: React.FC = () => {
 
 const App: React.FC = () => {
   useEffect(() => {
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-    });
-
-    messaging()
-      .getToken()
-      .then((token) => {
-        // console.log(token)
+    messaging().onMessage(async (remoteMessage) => {
+      await notifee.createChannel({
+        id: 'notification',
+        name: 'Firing notification',
+        lights: false,
+        vibration: true,
+        importance: AndroidImportance.DEFAULT,
       });
 
-    return unsubscribe;
+      await Toast.show({
+        position: 'top',
+        title: remoteMessage.notification.title,
+        text: remoteMessage.notification.body,
+        color: '#2ecc71',
+        duration: 5000,
+    })
+    });
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      // The below code gets never executed
+      NavigationService.navigate("ActivityList");
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage,
+      );
+    });
+
+    messaging().getToken().then((token) => {
+        console.log("firebase token:", token)
+      });
   }, []);
 
   return (
     <Provider store={store}>
       <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+      <Root>
         <RootNavigation />
+    </Root>
       </PersistGate>
     </Provider>
   );
